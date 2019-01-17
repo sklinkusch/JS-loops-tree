ad6aaa99f1b9f0669e291b4cfa02c24e6a692cc6
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-17 10:20:21 +0100

    wrote it into a div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 function TreeAsterisks(max) {
   let row = "";
+  let total = "";
   for (let x = 1; x <= max; x++) {
     row = row + "*";
+    total = total + row + "<br>";
     console.log(row);
-    document.write(row + "<br>");
   }
+  document.getElementById("asterisks").innerHTML = total;
 }
 TreeAsterisks(20);
